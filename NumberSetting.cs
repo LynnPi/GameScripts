a88980a725d0c2403839f4c03f16c395a5e7561f
LynnPi <18681276745@163.com>
2015-08-10 13:43:50 +0800

    Add LongPress And Number Animation Scripts

---
+++ NumberSetting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using NumberExtension;
+
+public class NumberSetting : MonoBehaviour {
+	Text _text ;
+
+	ulong _value;
+	public ulong Number{
+		set{
+			DebugUtils.Assert(_findText != null, "The delegate : _findText == null");
+			DebugUtils.Assert(_text != null, "Text == NULL !");
+			_value = value;
+			_text.text = value.ToThousandFormatString();
+		}
+		get {
+			return _value;
+		}
+	}
+
+	public delegate Text FindTextHandler();
+	FindTextHandler _findText;
+	public FindTextHandler FindText{
+		set{
+			_findText = value; 
+			_text = _findText();
+		} 
+	}
+}
