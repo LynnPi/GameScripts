c76791a7a917408c6d84708d4bae6a07f4868e92
LynnPi <18681276745@163.com>
2015-08-10 18:37:43 +0800

    Add RotateObjectBasedInput

---
+++ RotateObjectBasedInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class RotateObjectBasedInput : MonoBehaviour {
+    public GameObject RotateTarget;
+
+    private bool _isRotating;
+
+    private void Start() {
+        RotateTarget = gameObject;
+    }
+    private void Update() {
+        if (RotateTarget && _isRotating) {
+            var rotation = Quaternion.Euler(0, -Input.mousePosition.x, 0);
+            RotateTarget.transform.rotation = rotation;
+        }
+    }
+
+
+    private void OnMouseDown() {
+        _isRotating = true;
+    }
+
+    private void OnMouseUp() {
+        _isRotating = false;
+    }
+}
